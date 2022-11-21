73bff06a4a70f44053083f08415902ce3924857a
dedev5 <dedev5430@gmail.com>
2022-11-20 21:47:22 -0500

    Adding dislikes to new tuits

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -11,6 +11,7 @@ const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
+    newTuit.dislikes = 0;
     newTuit.liked = false;
     newTuit.handle = currentUser.handle
     newTuit.username = currentUser.username
